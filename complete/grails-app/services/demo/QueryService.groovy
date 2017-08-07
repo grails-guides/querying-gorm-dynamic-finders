fe44eb0a3dc7c0b9832f457c5e8fe80ca740601f
Matthew Moss <mattmoss@splatbang.com>
2017-08-07 12:56:18 -0500

    Remove @Transactional; remove where queries; make dynamic finder queries active; change "all" to "list()"

--- complete/grails-app/services/demo/QueryService.groovy
+++ complete/grails-app/services/demo/QueryService.groovy
@@ -1,46 +1,29 @@
 package demo
 
-import grails.transaction.Transactional
-
-@Transactional
 class QueryService {
 
     def findGamesRatedAtLeast(minRating) {
-//1        Game.all.findAll { it.rating >= minRating }
-//2        Game.findAllByRatingGreaterThanEquals(minRating)
+        // Game.list().findAll { it.rating >= minRating }
 
-        Game.where {
-            rating >= minRating
-        }.list()
+        Game.findAllByRatingGreaterThanEquals(minRating)
     }
 
     def findGamesForPlayerCount(int playerCount) {
-//1        Game.all.findAll { it.minPlayers <= playerCount && playerCount <= it.maxPlayers }
-//2        Game.findAllByMinPlayersLessThanEqualsAndMaxPlayersGreaterThanEquals(playerCount, playerCount)
+        // Game.list().findAll { it.minPlayers <= playerCount && playerCount <= it.maxPlayers }
 
-        Game.where {
-            minPlayers <= playerCount
-            maxPlayers >= playerCount
-        }.list()
+        Game.findAllByMinPlayersLessThanEqualsAndMaxPlayersGreaterThanEquals(playerCount, playerCount)
     }
 
     def findMatchesInProgress() {
-//1        Match.all.findAll { !it.finished }
-//2        Match.findAllByFinishedIsNull()
+        // Match.list().findAll { !it.finished }
 
-        Match.where {
-            finished == null
-        }.list()
+        Match.findAllByFinishedIsNull()
     }
 
     def findUniqueGamesPlayed() {
-//1        Match.all*.game.unique()
+        Match.list()*.game.unique()
 
-        Match.where {
-            projections {
-                distinct 'game'
-            }
-        }.list()
+        // TODO: Not possible with dynamic finder?
     }
 
 }
