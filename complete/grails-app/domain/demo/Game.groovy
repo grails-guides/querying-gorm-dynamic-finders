730a5d8a2131a788108f0015a1a304f590aba631
Matthew Moss <mattmoss@splatbang.com>
2017-08-25 16:21:42 -0500

    Add family/party/thematic/strategy flags. Better validator for maxPlayers.

--- complete/grails-app/domain/demo/Game.groovy
+++ complete/grails-app/domain/demo/Game.groovy
@@ -6,13 +6,17 @@ class Game {
     Integer maxPlayers
     Integer aveDuration     // in minutes
     BigDecimal rating       // from 0.0 to 10.0
+    Boolean family = false
+    Boolean party = false
+    Boolean thematic = false
+    Boolean strategy = false
 
     static hasMany = [categories: Category, mechanics: Mechanic]
 
     static constraints = {
         name blank: false
         minPlayers min: 1
-        maxPlayers min: 1
+        maxPlayers validator: { val, obj -> val >= obj.minPlayers }
         aveDuration min: 1
         rating min: 0.0, max: 10.0
     }
