69b7421b260cdfc5460c507cbd4c6cd688b85dfe
Matthew Moss <mattmoss@splatbang.com>
2017-08-30 15:41:39 -0500

    (complete) show dynamic finder solution alongside cpu/memory solution (commented out)

--- complete/grails-app/services/demo/QueryService.groovy
+++ complete/grails-app/services/demo/QueryService.groovy
@@ -5,155 +5,155 @@ import org.apache.commons.lang.StringUtils
 class QueryService {
 
     def queryGame(String name) {
-        Game.list().find {
-            it.name == name
-        }
+        //Game.all.find {
+        //    it.name == name
+        //}
 
-        //Game.findByName(name)
+        Game.findByName(name)
     }
 
     def queryGamesWithAverageDuration(Integer averageDuration) {
-        Game.list().findAll {
-            it.aveDuration == averageDuration
-        }
+        //Game.all.findAll {
+        //    it.aveDuration == averageDuration
+        //}
 
-        //Game.findAllByAveDuration(averageDuration)
+        Game.findAllByAveDuration(averageDuration)
     }
 
     def queryGamesExpectedShorterThan(Integer duration) {
-        Game.list().findAll {
-            it.aveDuration <= duration
-        }
+        //Game.all.findAll {
+        //    it.aveDuration <= duration
+        //}
 
-        //Game.findAllByAveDurationLessThanEquals(duration)
+        Game.findAllByAveDurationLessThanEquals(duration)
     }
 
     def queryGamesRatedMoreThan(BigDecimal rating) {
-        Game.list().findAll {
-            it.rating > rating
-        }
+        //Game.all.findAll {
+        //    it.rating > rating
+        //}
 
-        //Game.findAllByRatingGreaterThan(rating)
+        Game.findAllByRatingGreaterThan(rating)
     }
 
     def queryHowManyGamesRatedAtLeast(BigDecimal rating) {
-        Game.list().count {
-            it.rating >= rating
-        }
+        //Game.all.count {
+        //    it.rating >= rating
+        //}
 
-        //Game.countByRatingGreaterThanEquals(rating)
+        Game.countByRatingGreaterThanEquals(rating)
     }
 
     def queryMatchesPlayedBetweenDates(Date startDate, Date finishDate) {
-        Match.list().findAll {
-            startDate <= it.started && it.started <= finishDate
-        }
+        //Match.all.findAll {
+        //    startDate <= it.started && it.started <= finishDate
+        //}
 
-        //Match.findAllByStartedBetween(startDate, finishDate)
+        Match.findAllByStartedBetween(startDate, finishDate)
     }
 
     def queryPlayersWithLastName(String lastName) {
-        Player.list().findAll {
-            it.name.endsWith " ${lastName}"
-        }
+        //Player.all.findAll {
+        //    it.name.endsWith " ${lastName}"
+        //}
 
-        //Player.findAllByNameLike("% ${lastName}")
+        Player.findAllByNameLike("% ${lastName}")
     }
 
     def queryHowManyScoresWithinRange(Range range) {
-        Score.list().count {
-            it.score in range
-        }
+        //Score.all.count {
+        //    it.score in range
+        //}
 
-        //Score.countByScoreInRange(range)
+        Score.countByScoreInRange(range)
     }
 
     def queryMechanicsContaining(String text) {
-        Mechanic.list().findAll {
-            StringUtils.containsIgnoreCase it.name, text
-        }
+        //Mechanic.all.findAll {
+        //    StringUtils.containsIgnoreCase it.name, text
+        //}
 
-        //Mechanic.findAllByNameIlike("%${text}%")
+        Mechanic.findAllByNameIlike("%${text}%")
     }
 
     def queryGamesMatching(String pattern) {
-        Game.list().findAll {
-            it.name ==~ pattern
-        }
+        //Game.all.findAll {
+        //    it.name ==~ pattern
+        //}
 
-        // Game.findAllByNameRlike(pattern)     // Rlike: limited to MySQL/Oracle
+        Game.findAllByNameRlike(pattern)      // Rlike: not universally supported
     }
 
     def queryHowManyMatchesInProgress() {
-        Match.list().count {
-            it.finished == null
-        }
+        //Match.all.count {
+        //    it.finished == null
+        //}
 
-        //Match.countByFinishedIsNull()
+        Match.countByFinishedIsNull()
     }
 
     def queryHowManyMatchesCompleted() {
-        Match.list().count {
-            it.finished != null
-        }
+        //Match.all.count {
+        //    it.finished != null
+        //}
 
-        //Match.countByFinishedIsNotNull()
+        Match.countByFinishedIsNotNull()
     }
 
     def queryHowManyGamesSupportPlayerCount(Integer playerCount) {
-        Game.list().count {
-            it.minPlayers <= playerCount && playerCount <= it.maxPlayers
-        }
+        //Game.all.count {
+        //    it.minPlayers <= playerCount && playerCount <= it.maxPlayers
+        //}
 
-        //Game.countByMinPlayersLessThanEqualsAndMaxPlayersGreaterThanEquals(playerCount, playerCount)
+        Game.countByMinPlayersLessThanEqualsAndMaxPlayersGreaterThanEquals(playerCount, playerCount)
     }
 
     def queryGamesSupportExactPlayerCount(Integer playerCount) {
-        Game.list().findAll {
-            it.minPlayers == playerCount && it.maxPlayers == playerCount
-        }
+        //Game.all.findAll {
+        //    it.minPlayers == playerCount && it.maxPlayers == playerCount
+        //}
 
-        //Game.findAllByMinPlayersAndMaxPlayers(playerCount, playerCount)
+        Game.findAllByMinPlayersAndMaxPlayers(playerCount, playerCount)
     }
 
     def queryGamesForNames(List<String> names) {
-        Game.list().findAll {
-            it.name in names
-        }
+        //Game.all.findAll {
+        //    it.name in names
+        //}
 
-        //Game.findAllByNameInList(names)
+        Game.findAllByNameInList(names)
     }
 
     def queryMatchesForGames(List<Game> games) {
-        Match.list().findAll {
-            it.game in games
-        }
+        //Match.all.findAll {
+        //    it.game in games
+        //}
 
-        //Match.findAllByGameInList(games)
+        Match.findAllByGameInList(games)
     }
 
     def queryGamesConsideredFamilyOrParty() {
-        Game.list().findAll {
-            it.family || it.party
-        }
+        //Game.all.findAll {
+        //    it.family || it.party
+        //}
 
-        //Game.findAllByFamilyOrParty(true, true)
+        Game.findAllByFamilyOrParty(true, true)
     }
 
     def queryHowManyGamesNotConsideredStrategy() {
-        Game.list().count {
-            !it.strategy
-        }
+        //Game.all.count {
+        //    !it.strategy
+        //}
 
-        //Game.countByStrategyNotEqual(true)
+        Game.countByStrategyNotEqual(true)
     }
 
     def queryGamesOtherThan(List<Game> games) {
-        Game.list().findAll {
-            !(it in games)
-        }
+        //Game.all.findAll {
+        //    !(it in games)
+        //}
 
-        //Game.findAllByNameNotInList(games*.name)
+        Game.findAllByNameNotInList(games*.name)
     }
 
 }
